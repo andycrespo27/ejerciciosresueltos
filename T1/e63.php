7b0568003b8b0a9567966a8987fd582a93dab973
Andres <andy.crespo@gmail.com>
2015-09-29 16:33:27 +0200

    ejercicio del tema 1

---
+++ T1/e63.php
@@ -0,0 +1,14 @@
+<?php 
+	$numero=mt_rand(100,200);
+	
+	$valor=&$numero;
+    
+    ?>
+    
+   <html>
+    
+    <p><b><u>Numero aleatorio entre 100 y 200 ambos incluidos</u></b></p>
+    
+    <h1><?php echo $numero ?></h1>    
+    
+    </html>
